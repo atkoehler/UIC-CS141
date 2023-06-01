1fa0fb6fced07dd40f2190bc9a93387b250b2695
Adam T Koehler, PhD <akoehler@uic.edu>
2023-06-01 10:55:28 -0500

    initial June 1 notes

---
+++ notes/20230601_LiveSession9.cpp
@@ -0,0 +1,14 @@
+/// @file 20230601_LiveSession9.cpp
+/// @author Adam T Koehler, PhD
+/// @date June 1, 2023
+/// @brief Code and lecture notes from the live session.
+
+#include <iostream>   
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
