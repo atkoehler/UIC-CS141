4a46e5b3e8834319e353766eaad9bbf8a4703fac
Adam T Koehler, PhD <akoehler@uic.edu>
2023-06-06 16:47:34 -0500

    updated brief to remove recursion and add actual coverage

--- notes/20230606/plant.h
+++ notes/20230606/plant.h
@@ -2,8 +2,8 @@
 /// @author Adam T Koehler, PhD
 /// @date June 6, 2023
 /// @brief Class code and lecture notes from the live session. Building on
-///        June 1st notes and implementation to fully build a class with 
-///        recursion examples.
+///        June 1st notes and implementation to fully build a class  
+///        with operator=, copy constructor, and destructor.
 
 // Copyright Notice
 // This document is protected by U.S. copyright law. Reproduction and 
