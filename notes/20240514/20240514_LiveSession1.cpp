63463ed40191ce0ad1ee1cc85795cba64ae7fc35
Adam T Koehler, PhD <akoehler@uic.edu>
2024-05-16 09:09:04 -0500

    notes from may 14, not commented or cleaned

--- notes/20240514/20240514_LiveSession1.cpp
+++ notes/20240514/20240514_LiveSession1.cpp
@@ -1,4 +1,59 @@
-/// @file 20230514_LiveSession1.cpp
+/// @file 20240514_LiveSession1.cpp
 /// @author Adam T Koehler, PhD
 /// @date May 14, 2024
 /// @brief Code and lecture notes from the live session.
+
+
+// libraries to include, we always want to be specific
+#include <iostream>
+#include <string>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    // identify a specific two character for each card
+    // then storing in a string, separated by a space
+    // e.g. "?B AH"
+
+    // "B-9 R+4"
+
+    string values = "A1234567890JQK?";
+
+    string userEntry, userEntry2;
+    char userE;
+    int uE; // index into the string
+
+    // cout << (int) 'A' << endl;
+
+    cin >> userE;
+    // cin >> userEntry;
+    // getline(cin, userEntry2);
+    // cin >> uE;
+
+    // cout << userE << endl;
+    // cout << userEntry << endl;
+
+    // cout << userEntry2 << endl;
+    // cout << uE << endl;
+
+    int randIndex;
+    srand(100);
+    randIndex = rand()  % values.size();
+
+    cout << (values.at(randIndex)) << endl;
+
+    if (values.at(randIndex) < userE)
+    {
+        cout << "You win!" << endl;
+    }
+    else
+    {
+        cout << "You Lose :(" << endl;
+    }
+}
+
+
+
+
