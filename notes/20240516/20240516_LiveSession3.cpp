8ebb448b3a620afe4b759ed6ba190b5b90ac1a31
Adam T Koehler, PhD <akoehler@uic.edu>
2024-05-16 09:10:37 -0500

    initial notes push from may 16

---
+++ notes/20240516/20240516_LiveSession3.cpp
@@ -0,0 +1,32 @@
+/// @file 20240516_LiveSession3.cpp
+/// @author Adam T Koehler, PhD
+/// @date May 16, 2024
+/// @brief Code and lecture notes from the live session.
+
+
+// libraries to include, we always want to be specific
+#include <iostream>
+#include <string>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+int row, col;
+for (row=0; row < 2; ++row) 
+{
+   cout << "x";
+   for (col=0; col < 3; ++col)
+   {
+      // do something
+   }
+}
+
+    return 0;
+}
+
+
+
+
+
