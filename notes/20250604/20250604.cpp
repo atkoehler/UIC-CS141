073c478b658913bd4c831a0722a6cc67e2699ff2
Adam T Koehler, PhD <akoehler@uic.edu>
2025-05-31 00:45:52 -0500

    push of new notes directories

---
+++ notes/20250604/20250604.cpp
@@ -0,0 +1,57 @@
+/// @file 20250604.cpp
+/// @author Adam T Koehler, PhD
+/// @date June 4, 2025
+/// @brief Code and lecture notes from the live session. 
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "card.h"
+
+using namespace std;
+
+void Examples1();
+void Examples2();
+void Examples3();
+
+/// @brief main function for running our examples
+/// @param argc the number of command line arguments
+/// @param argv the array containing c-strings of command line args
+int main(int argc, char *argv[])
+{
+    // no second command line argument, run all examples
+    if (argc > 1 && isdigit(argv[1][0]))
+    {       
+        switch(atoi(argv[1]))
+        {
+            case 1:
+                Examples1();   
+                break;
+
+            case 2:
+                Examples2();
+                break;
+
+            case 3:
+                Examples3();
+                break;
+            
+            default:
+                break;
+        }
+    }
+    else
+    {
+        // could not parse or did not find extra command line argument
+        // therefore just run everything
+        Examples1();
+
+        cout << endl << endl;
+        Examples2();
+        
+        cout << endl << endl;  
+        Examples3();  
+        
+    }    
+    return 0;
+}
