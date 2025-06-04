15a9a2d6ea42200b8a1b75c1d783be1e4a8dfc65
Adam T Koehler, PhD <akoehler@uic.edu>
2025-06-04 11:57:28 -0500

    post lecture push

--- notes/20250604/20250604.cpp
+++ notes/20250604/20250604.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <vector>
 #include "card.h"
+#include "deck.h"
 
 using namespace std;
 
@@ -54,4 +55,138 @@ int main(int argc, char *argv[])
         
     }    
     return 0;
+}
+
+
+void Examples1()
+{
+    char a[6] = "hello";
+    char b[81] = "hello world";
+    char c[81] = "hello my name is";
+    char d[81] = "hello\nworld";
+
+    // Note the counts are the quantity of characters NOT the array sizes
+
+}
+
+void Examples2()
+{
+    Card A("Jack", "Spades", false);
+    Card B("Jack", "Hearts", true);
+    Card C("King", "Spades", false);
+
+
+
+    if (A > B)
+    {
+        cout << "A is larger than B" << endl;
+    }
+    else
+    {
+        cout << "A is not larger than B" << endl;
+    }
+
+    if (A > C)
+    {
+        cout << "A is larger than C" << endl;
+    }
+    else
+    {
+        cout << "A is not larger than C" << endl;
+    }
+
+}
+
+void Examples3()
+{
+
+}
+
+
+
+// Similar to when we pass by reference we have to decide const or not
+// A pointer contains a memory address, so when we do not specificy const
+// then we may modify the contents at the memory address the pointer points to
+// size_t countbyPtr(const char *cPtr)
+
+
+/// @brief Count the quantity of characters that exist in provided C-string
+/// @param cArr C-string to start counting from
+/// @return the quantity of characters counted
+int countC(const char str[])
+{
+    int sum = 0;
+    int index = 0;
+
+    sum = 0;
+    while(str[index] != '\0')
+    {
+        sum++;
+        index++;
+    }
+
+    sum = 0;
+    for(index=0; str[index] != '\0'; index++)
+    { 
+        sum++;
+    }
+
+    // index = 0;
+    // while(str[index] != '\0')
+    // {
+    //     index++;
+    // }
+    // return index;
+
+    // index = 0;
+    // while(str[index++] != '\0');
+    // return index;
+
+    return sum;
+}
+
+
+
+/// @brief Count the quantity of characters that exist in provided C-string
+///     Use pointer dereferencing and pointer parameter for this version
+/// @param cPtr pointer to address in a C-string to start counting from
+/// @return the quantity of characters counted
+int countC_PtrParam(const char *str)
+{
+    int sum = 0;
+    int index = 0;
+
+    sum = 0;
+    while(str[index] != '\0')
+    {
+        sum++;
+        index++;
+    }
+
+    return sum;
+}
+
+
+/// @brief Count the quantity of characters that exist in provided C-string
+///     Use pointer dereferencing and pointer parameter for this version
+///     Also use pointer math, not a counter, to calculate the difference
+///     in memory addresses to calculate the quantity of characters.
+/// @param cPtr pointer to address in a C-string to start counting from
+/// @return the quantity of characters counted
+int countC_PtrParam(char *start)
+{
+    char *stop = start;
+
+    // *loopVar - we get the character at that memory address
+    while(*stop != '\0')
+    {
+        stop++; 
+        // move 1 sizeof() type of pointer further in memory    
+    }
+
+
+    // int length = (stop - start) / sizeof(char);
+    // length = (stop - start) / 1
+
+    return stop - start;
 }
---
+++ notes/20250604/deck.cpp
@@ -0,0 +1,95 @@
+/// @file deck.cpp
+/// @author Adam T Koehler, PhD
+/// @date June 3, 2025
+/// @brief Deck class that is continually under development.
+///     New functionality that is added in a lecture will exist at end
+///     after comment about lecture date that featured the live additions.
+
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+#include "card.h"
+#include "deck.h"
+
+using namespace std;
+
+// =====================================================
+// Functionality implementations from 06-03-2025 lecture
+// =====================================================
+
+/// @brief combine all elements of the deck
+///         into a single string separated
+///         by newline
+/// @return single string with all the elements
+string Deck::to_string() const
+{
+    string newString; 
+
+    // index based for loop
+    // for each is the natural choice
+    // going through all the elements
+    // and we don't care about the index
+    for(const Card& c : dk)
+    {
+        newString += c.to_string() + "\n";
+    }
+    
+    
+    // "Card is a Jack of Spades which is black\n
+    //  Card is a Jack of Hearts which is red\n
+    //  ";
+    if (dk.size() > 0)
+    {
+        newString.pop_back();
+    }
+
+    return newString;
+}
+
+/// @brief sends the object's string conversion to output
+void Deck::printDeck() const
+{
+    cout << to_string() << endl;
+}
+
+
+// ===========================================================================
+// Functionality implementations asynchronously added after 06-03-2025 lecture
+// ===========================================================================
+
+/// @brief provide the quantity of cards remaining in the deck using the container size
+/// @return the quantity of cards remaining in the deck
+size_t Deck::deckSize() const
+{
+    return dk.size();
+}
+
+/// @brief checks if the deck is empty or not
+/// @return true when the deck is empty, false otherwise
+bool Deck::empty() const
+{
+    return dk.empty();
+}
+
+
+void  Deck::addCardBottom(const Card &c)
+{
+    dk.push_back(c);
+}
+
+Card Deck::drawRandom()
+{
+    int randomNum = 0 + rand()%deckSize();
+
+    Card retVal = dk.at(randomNum);
+
+    for(int i=randomNum; i+1 < deckSize(); ++i)
+    {
+        dk.at(i) = dk.at(i+1);
+    }
+    dk.pop_back();
+
+    return retVall
+}
+
